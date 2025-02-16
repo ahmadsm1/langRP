67fd9e48a175d760599297429ee6eb4ee3776bfa
ahmadsm1 <ahmad.mirza@hotmail.com>
2025-02-16 15:33:17 -0800

    Add logo

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,43 +1,23 @@
 'use client'
 
-import { toast } from "sonner"
 import { CardWithForm } from "./menu_card";
+import localFont from 'next/font/local'
 
-async function fetchHello(): Promise<string | null> {
-  try {
-    const response = await fetch("http://localhost:8000/");
-    if (!response.ok){
-      throw new Error("Network response error");
-    }
-    const data = await response.text();
-    return data;
-    
-  } catch (error) {
-    console.log("Error bro:", error);
-    return null;
-  }
-}
+const specialFont = localFont({
+  src: '../public/fonts/Noe-Display-Bold.ttf',
+  display: 'swap',
+})
 
 export default function Home() {
 
-  const handleClick = async () => {
-    const data = await fetchHello();
-    console.log(data);
-    if (data){
-      toast("Message received:", {
-        description: data,
-        action: {
-          label: "Undo",
-          onClick: () => console.log("Undo"),
-        },
-      });
-    }
-  }
 
   return (
     <main>
       <div className="flex h-screen">
         <div className="m-auto">
+          <div className={`${specialFont.className} text-5xl font-bold text-center`}>
+                langRP
+          </div>
           <CardWithForm/>
         </div>
       </div>
